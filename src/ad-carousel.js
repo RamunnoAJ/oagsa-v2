844d2ab1693a0637902a43d3e1ea6488c9df6aeb
Stefania Sanchez Golfieri <sv.sanchezgolfieri@gmail.com>
2023-01-02 13:18:09 -0300

    started advertisement-banner

---
+++ src/ad-carousel.js
@@ -0,0 +1,6 @@
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
