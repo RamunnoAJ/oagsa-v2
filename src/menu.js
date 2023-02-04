055cf8aef1fdef1cadc0ebb0c02c6d82e3abc076
RamunnoAJ <ramunnoagustin@gmail.com>
2023-02-04 13:44:35 -0300

    add: format and linter to the project

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,8 @@
-const $btnMenu = document.querySelector('.btn-menu');
-const $body = document.body;
-const $navBar = document.querySelector('.nav-bar');
+const $btnMenu = document.querySelector('.btn-menu')
+const $body = document.body
+const $navBar = document.querySelector('.nav-bar')
 
 $btnMenu.addEventListener('click', () => {
-  $body.classList.toggle('menu-open');
-  $navBar.classList.toggle('active');
-});
+  $body.classList.toggle('menu-open')
+  $navBar.classList.toggle('active')
+})
