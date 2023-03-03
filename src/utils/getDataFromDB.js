5d953bd7fe731f763a0b47d11bf4467c94cbdd5a
RamunnoAJ <ramunnoagustin@gmail.com>
2023-03-03 13:11:29 -0300

    add: styles to dashboard page
    add some ids to the html and the styles for the client list section

--- src/utils/getDataFromDB.js
+++ src/utils/getDataFromDB.js
@@ -1,6 +1,7 @@
-export const getDataFromDB = async url => {
+const getDataFromDB = async url => {
   const response = await fetch(url)
   const data = await response.json()
-  console.log(data)
   return data
 }
+
+export default getDataFromDB
