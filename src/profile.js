94c6f381dcdc7e51ae8b0b463598aa3304eebfbb
RamunnoAJ <ramunnoagustin@gmail.com>
2023-03-03 13:12:36 -0300

    add: client list render
    add the functions to render the different client list objects, sorting and filtering them

--- src/profile.js
+++ src/profile.js
@@ -1,7 +1,23 @@
-const getDataFromDB = async url => {
-  const response = await fetch(url)
-  const data = await response.json()
-  return data
-}
+import { profileClientList } from './profileClientList.js'
 
-getDataFromDB('http://api.oagsa.com/api/cliente/vendedor?pVendedor=1')
+const $profileTitle = document.querySelector('#profileTitle')
+const $profileList = document.querySelector('#profileList')
+const $profileInfoContainer = document.querySelector('#profileInfoContainer')
+
+$profileList.addEventListener('click', e => {
+  if (e.target.closest('li') === null) return
+  $profileTitle.textContent = e.target.textContent
+
+  switch ($profileTitle.textContent) {
+    case 'Lista de clientes':
+      $profileInfoContainer.innerHTML = ''
+      profileClientList($profileInfoContainer)
+      break
+
+    case 'Precarga de clientes':
+      break
+    default:
+      $profileInfoContainer.innerHTML = ''
+      break
+  }
+})
