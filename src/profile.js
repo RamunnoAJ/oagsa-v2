d2e76d6d656e5c1779d7654f0cea3b077a0beb09
RamunnoAJ <ramunnoagustin@gmail.com>
2023-03-09 15:26:13 -0300

    save login data in the browser

--- src/profile.js
+++ src/profile.js
@@ -4,6 +4,23 @@ import { profileClientList } from './profileClientList.js'
 const $profileTitle = document.querySelector('#profileTitle')
 const $profileList = document.querySelector('#profileList')
 const $profileInfoContainer = document.querySelector('#profileInfoContainer')
+const $logoutBtn = document.querySelector('#logout-btn')
+
+const localStorageID = Number(localStorage.getItem('sessionID'))
+const sessionStorageID = Number(sessionStorage.getItem('sessionID'))
+
+if (localStorageID === 0 && sessionStorageID === 0) {
+  window.location.replace('../pages/log-in.html')
+}
+
+const logOut = () => {
+  localStorage.removeItem('sessionID')
+  sessionStorage.removeItem('sessionID')
+
+  window.location.replace('../pages/log-in.html')
+}
+
+$logoutBtn.addEventListener('click', logOut)
 
 $profileList.addEventListener('click', e => {
   if (e.target.closest('li') === null) return
