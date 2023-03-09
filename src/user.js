0305aa935ca91b764a3d2b4ac72337d89ba78a31
RamunnoAJ <ramunnoagustin@gmail.com>
2023-03-09 15:26:37 -0300

    render icon depending in if the user is logged

---
+++ src/user.js
@@ -0,0 +1,19 @@
+const sessionStorageID = sessionStorage.getItem('sessionID')
+const localStorageID = localStorage.getItem('sessionID')
+const currentURL = window.location.href
+const $accountIcon = document.querySelector('#account-icon')
+
+let buttonURL = './pages/dashboard.html'
+
+if (currentURL.includes('/pages/')) {
+  buttonURL = './dashboard.html'
+}
+
+if (localStorageID || sessionStorageID) {
+  $accountIcon.innerHTML = `
+  <div>
+  <button class="button button-sm mt-1 bg-white bg-hover-slate"><i class="fa-solid fa-envelope"></i></button>
+  <a href=${buttonURL} class="button button-sm mt-1 bg-secondary-300 bg-hover-secondary-400 text-black"><i class="fa-solid fa-user"></i></a>
+  </div>
+  `
+}
