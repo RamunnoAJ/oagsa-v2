620fa06a707cc8bdc046f8f0d24d6d6dfa5516f4
RamunnoAJ <ramunnoagustin@gmail.com>
2023-03-10 12:36:58 -0300

    add: throw error when fetch doesn't work

--- src/utils/getDataFromDB.js
+++ src/utils/getDataFromDB.js
@@ -1,6 +1,11 @@
 const getDataFromDB = async url => {
   const response = await fetch(url)
   const data = await response.json()
+
+  if (!response.ok) {
+    throw new Error('Respuesta rechazada')
+  }
+
   return data
 }
 
