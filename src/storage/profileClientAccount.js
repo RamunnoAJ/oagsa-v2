ecf2922495b67c0864a139d9ad18f9f0632d1864
RamunnoAJ <ramunnoagustin@gmail.com>
2023-05-24 13:05:20 -0300

    fix: cookie session

--- src/storage/profileClientAccount.js
+++ src/storage/profileClientAccount.js
@@ -1,14 +1,14 @@
 import { getClientsFromSeller } from '../api/profileClientList.js'
 import getCookie from './storageData.js'
 
-const userFromCookie = JSON.parse(getCookie('user'))
-
 export function getStorageID() {
+  const userFromCookie = JSON.parse(getCookie('user'))
   if (userFromCookie) return userFromCookie.id
   return null
 }
 
 export async function getDataFromStorage(sellerID) {
+  const userFromCookie = JSON.parse(getCookie('user'))
   if (userFromCookie) {
     if (userFromCookie.role === 1) {
       return await getClientsFromSeller('all')
