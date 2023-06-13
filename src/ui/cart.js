f97babb61f727a4954b8d4baf2038ce135641579
RamunnoAJ <ramunnoagustin@gmail.com>
2023-06-13 14:45:46 -0300

    add: functionality to cart

--- src/ui/cart.js
+++ src/ui/cart.js
@@ -1,7 +1,10 @@
-export function showToast() {
+import { emptyCart, getTotalPrice, removeFromCart } from '../cart.js'
+import { getCart } from '../storage/cart.js'
+
+export function showToast(message) {
   // eslint-disable-next-line no-undef
   Toastify({
-    text: 'Objeto añadido correctamente',
+    text: message,
     duration: 3000,
     close: false,
     gravity: 'top',
@@ -12,4 +15,105 @@ export function showToast() {
       color: '#000000',
     },
   }).showToast()
+}
+
+if (window.location.href.includes('cart')) {
+  const cart = getCart() || []
+  renderCart(cart)
+}
+
+function renderCart(cart) {
+  const $cart = document.getElementById('cart')
+  const $table = createTable()
+  $cart.innerHTML = ''
+
+  cart.forEach(item => {
+    const $row = createProductRow(item)
+    $table.appendChild($row)
+  })
+
+  $cart.appendChild($table)
+  $cart.appendChild(createTotalAmount())
+
+  renderEmptyCart()
+}
+
+function renderEmptyCart() {
+  const $cart = document.getElementById('cart')
+
+  const $emptyCartButton = document.createElement('button')
+  $emptyCartButton.textContent = 'Vaciar carrito'
+  $emptyCartButton.addEventListener('click', () => {
+    emptyCart()
+    renderCart(getCart())
+  })
+
+  $cart.appendChild($emptyCartButton)
+}
+
+function createTotalAmount() {
+  const $totalAmount = document.createElement('div')
+  $totalAmount.classList.add('total-amount')
+  $totalAmount.textContent = `$${getTotalPrice()}`
+  return $totalAmount
+}
+
+function createTable() {
+  const $table = document.createElement('table')
+  $table.classList.add('cart__table')
+
+  const $head = document.createElement('thead')
+  const $row = document.createElement('tr')
+  const $article = document.createElement('th')
+  const $description = document.createElement('th')
+  const $price = document.createElement('th')
+  const $quantity = document.createElement('th')
+  const $total = document.createElement('th')
+  const $delete = document.createElement('th')
+  $article.textContent = 'Artículo'
+  $description.textContent = 'Descripción'
+  $price.textContent = 'Precio'
+  $quantity.textContent = 'Cantidad'
+  $total.textContent = 'Total'
+  $row.appendChild($article)
+  $row.appendChild($description)
+  $row.appendChild($price)
+  $row.appendChild($quantity)
+  $row.appendChild($total)
+  $row.appendChild($delete)
+  $head.appendChild($row)
+  $table.appendChild($head)
+
+  return $table
+}
+
+function createProductRow(item) {
+  const $row = document.createElement('tr')
+  const $article = document.createElement('td')
+  const $description = document.createElement('td')
+  const $price = document.createElement('td')
+  const $quantity = document.createElement('td')
+  const $total = document.createElement('td')
+  const $delete = document.createElement('td')
+  $article.textContent = item.codigoArticulo
+  $description.textContent = item.descripcion
+  $price.textContent = `$${item.precio}`
+  $quantity.textContent = item.quantity
+  $total.textContent = `$${item.precio * item.quantity}`
+  $delete.textContent = '✕'
+  $row.appendChild($article)
+  $row.appendChild($description)
+  $row.appendChild($price)
+  $row.appendChild($quantity)
+  $row.appendChild($total)
+  $row.appendChild($delete)
+
+  $delete.addEventListener('click', () => {
+    removeFromCart(item)
+    renderCart(getCart())
+
+    showToast('Objeto eliminado correctamente.')
+  })
+
+  return $row
 }
