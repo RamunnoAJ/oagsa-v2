0992131a3aa101ff59c609fb28769ff341f9e35f
RamunnoAJ <ramunnoagustin@gmail.com>
2023-07-13 12:42:14 -0300

    add: conditional rendering based on user role

--- src/profile.js
+++ src/profile.js
@@ -3,18 +3,31 @@ import { profileClientList } from './profileClientList.js'
 import { profileDrafts } from './profileDrafts.js'
 import { profilePricesList } from './profilePricesList.js'
 import { checkLocalStorage } from './storage/profile.js'
+import { getUserFromStorage } from './storage/storageData.js'
 import { navigateToLogin } from './ui/login.js'
-import { renderAdminBtn } from './ui/profile.js'
 
 checkLocalStorage()
 
 const $profileTitle = document.querySelector('#profileTitle')
 const $profileList = document.querySelector('#profileList')
 const $profileInfoContainer = document.querySelector('#profileInfoContainer')
-const $btnContainer = document.querySelector('.profile-container__buttons')
+
 
 if (window.location.href.includes('dashboard')) {
-  renderAdminBtn($btnContainer)
+  $profileList.innerHTML = ''
+  $profileList.innerHTML += '<li>Cuenta corriente</li>'
+
+  const user = JSON.parse(getUserFromStorage())
+
+  if (user.role === 1) {
+    const itemsList = ['Lista de clientes', 'Lista de precios', 'Borrador de pedidos']
+
+    itemsList.forEach(item => {
+      $profileList.innerHTML += `<li>${item}</li>`
+    })
+  }
+
+  $profileList.innerHTML += `<li><a href="./store.html">Tienda</a></li>`
 
   $profileList.addEventListener('click', e => {
     if (e.target.closest('li') === null) return
--- src/ui/profile.js
+++ src/ui/profile.js
@@ -1,22 +1,11 @@
-import { getUserFromStorage } from '../storage/storageData.js'
-
 export function renderAdminBtn(parentElement) {
-  const user = getUserFromStorage()
-  if (user) {
-    const userFromCookie = JSON.parse(user)
-
-    if (userFromCookie) {
-      if (userFromCookie.role === 1) {
-        const adminBtn = document.createElement('p')
-        adminBtn.classList.add(
-          'button',
-          'bg-white',
-          'bg-hover-slate',
-          'uppercase'
-        )
-        adminBtn.textContent = 'Admin'
-        parentElement.appendChild(adminBtn)
-      }
-    }
-  }
+  const adminBtn = document.createElement('p')
+  adminBtn.classList.add(
+    'button',
+    'bg-white',
+    'bg-hover-slate',
+    'uppercase'
+  )
+  adminBtn.textContent = 'Admin'
+  parentElement.appendChild(adminBtn)
 }
