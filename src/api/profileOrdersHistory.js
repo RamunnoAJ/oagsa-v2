46afc13174b3a251ad61909b06d9e7d893cda069
RamunnoAJ <ramunnoagustin@gmail.com>
2023-07-20 13:56:48 -0300

    add: fetch orders by passing a client

--- src/api/profileOrdersHistory.js
+++ src/api/profileOrdersHistory.js
@@ -1,9 +1,8 @@
 import getDataFromDB from "../utils/getDataFromDB.js";
 
-export async function getOrders(id) {
-  const response = await getDataFromDB(`orden-compra/vendedor?pCodigoVendedor=${id}&pBorrador=0&pPageSize=1000`)
+export async function getOrders(id, idCliente = 0) {
+  const response = await getDataFromDB(`orden-compra/vendedor?pCodigoVendedor=${id}&pCodigoCliente=${idCliente}&pBorrador=0&pPageSize=1000`)
   const orders = await response.data
-  console.log(orders)
 
   return orders
 }
