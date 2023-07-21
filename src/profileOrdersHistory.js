473b7ad231851796e40060aaa7784df20030f095
RamunnoAJ <ramunnoagustin@gmail.com>
2023-07-21 13:39:21 -0300

    add: feature to filter orders by client

--- src/profileOrdersHistory.js
+++ src/profileOrdersHistory.js
@@ -1,12 +1,16 @@
+import { getClientsFromSeller } from "./api/profileClientList.js"
 import { getOrders } from "./api/profileOrdersHistory.js"
 import { getStorageID } from "./storage/profileClientAccount.js"
-import { renderOrders } from "./ui/profileOrdersHistory.js"
+import { renderOrders, renderSelect } from "./ui/profileOrdersHistory.js"
 
 export async function profileOrdersHistory(parentElement){
   parentElement.innerHTML = '<span class="loader"></span>'
 
   const sellerID = getStorageID()
   if (sellerID) {
+    const clients = await getClientsFromSeller(sellerID)
+    renderSelect(clients, parentElement)
+    
     const orders = await getOrders(sellerID)
     renderOrders(orders, parentElement)
   }
