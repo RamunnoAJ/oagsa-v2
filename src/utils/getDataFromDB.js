1ab7f78d928a626b1106513241b478c33b7a4447
RamunnoAJ <ramunnoagustin@gmail.com>
2023-07-24 13:36:43 -0300

    add: unit testing for utils/getDataFromDB.js

--- src/utils/getDataFromDB.js
+++ src/utils/getDataFromDB.js
@@ -1,6 +1,7 @@
 export const BASE_URL = 'http://api.oagsa.com/api/'
 
 export async function getDataFromDB(url) {
+  if (typeof url !== 'string') throw new Error('url must be a string')
   const response = await fetch(`${BASE_URL}${url}`)
   const data = await response.json()
 
