5c99c88bef524cfe75192f4d4b09d231005d0555
RamunnoAJ <ramunnoagustin@gmail.com>
2023-07-24 12:48:23 -0300

    add: setup for testing

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
-module.exports = {
+export default {
   verbose: true,
   rootDir: './src',
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}'],
   coverageDirectory: '../coverage/',
   testPathIgnorePatterns: ['./node_modules/', '.*fixture.js'],
   coveragePathIgnorePatterns: ['./node_modules/', '.*fixture.js'],
   resetMocks: false,
-  setupFiles: ['jest-localstorage-mock'],
+  setupFiles: ['jest-localstorage-mock', 'jest-environment-jsdom'],
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest',
