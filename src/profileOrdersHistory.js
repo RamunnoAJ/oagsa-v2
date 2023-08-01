cc889567f3de89ddaddb097450a5e91977bb8f0e
RamunnoAJ <ramunnoagustin@gmail.com>
2023-08-01 18:46:53 -0300

    update: pagination on the orders history

--- src/profileOrdersHistory.js
+++ src/profileOrdersHistory.js
@@ -1,17 +1,13 @@
-import { getClientsFromSeller } from "./api/profileClientList.js"
-import { getOrders } from "./api/profileOrdersHistory.js"
-import { getStorageID } from "./storage/profileClientAccount.js"
-import { renderOrders, renderSelect } from "./ui/profileOrdersHistory.js"
+import { getClientsFromSeller } from './api/profileClientList.js'
+import { getStorageID } from './storage/profileClientAccount.js'
+import { renderSelect } from './ui/profileOrdersHistory.js'
 
-export async function profileOrdersHistory(parentElement){
+export async function profileOrdersHistory(parentElement) {
   parentElement.innerHTML = '<span class="loader"></span>'
 
   const sellerID = getStorageID()
   if (sellerID) {
     const clients = await getClientsFromSeller(sellerID)
     renderSelect(clients, parentElement)
-    
-    const orders = await getOrders(sellerID)
-    renderOrders(orders, parentElement)
   }
 }
