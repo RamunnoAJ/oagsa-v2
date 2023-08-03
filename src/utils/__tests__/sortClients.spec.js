0d5ba3eb5f68c67b33cef33819f387b571f03ea8
RamunnoAJ <ramunnoagustin@gmail.com>
2023-08-03 16:22:58 -0300

    add: unit testing for utils/sortClients.js file

--- src/utils/__tests__/sortClients.spec.js
+++ src/utils/__tests__/sortClients.spec.js
@@ -2,10 +2,41 @@ import { sortClients } from '../sortClients.js'
 
 describe('sortClients', () => {
   test('should sort clients', () => {
-    const clients = [{ razonSocial: 2 }, { razonSocial: 5 }, { razonSocial: 4 }, { razonSocial: 3 }, { razonSocial: 1 }]
-    const sortedClients = [{ razonSocial: 1 }, { razonSocial: 2 }, { razonSocial: 3 }, { razonSocial: 4 }, { razonSocial: 5 }]
+    const clients = [
+      { razonSocial: 2 },
+      { razonSocial: 5 },
+      { razonSocial: 4 },
+      { razonSocial: 3 },
+      { razonSocial: 1 },
+    ]
+    const sortedClients = [
+      { razonSocial: 1 },
+      { razonSocial: 2 },
+      { razonSocial: 3 },
+      { razonSocial: 4 },
+      { razonSocial: 5 },
+    ]
 
     sortClients(clients)
     expect(clients).toEqual(sortedClients)
+  })
+
+  test('should sort clients without razonSocial', () => {
+    const clients = [
+      { nombre: 2 },
+      { nombre: 5 },
+      { nombre: 4 },
+      { nombre: 3 },
+      { nombre: 1 },
+    ]
+
+    sortClients(clients)
+    expect(clients).toEqual([
+      { nombre: 2 },
+      { nombre: 5 },
+      { nombre: 4 },
+      { nombre: 3 },
+      { nombre: 1 },
+    ])
   })
 })
