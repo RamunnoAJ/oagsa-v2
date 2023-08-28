695b69cc9cc30d138f6c20256f85bdec868e9660
RamunnoAJ <ramunnoagustin@gmail.com>
2023-08-28 13:32:09 -0300

    add: implement usage of mappers for the api calls

--- src/api/profilePricesList.js
+++ src/api/profilePricesList.js
@@ -1,20 +1,43 @@
+import { articlesMapper } from '../mappers/articles.js'
+import {
+  categoriesMapper,
+  classMaper,
+  subcategoriesMapper,
+} from '../mappers/categories.js'
 import { convertToUTF } from '../utils/convertToUTF.js'
 import getDataFromDB from '../utils/getDataFromDB.js'
 
 export async function getProducts(url) {
   const response = await getDataFromDB(url)
-  const products = await response.data
-  await products.forEach(product => {
+  const productsApi = await response.data
+  await productsApi.forEach(product => {
     product.descripcion = convertToUTF(product.descripcion)
     product.marca = convertToUTF(product.marca)
   })
 
+  const products = productsApi.map(product => articlesMapper(product))
+
   return products
 }
 
+/**
+ * @param {string} url
+ * @return {object}
+ * */
 export async function getCategories(url) {
   const response = await getDataFromDB(url)
-  const categories = await response.data
+  const categoriesApi = await response.data
+  let categories
+
+  if (url.includes('all')) {
+    categories = categoriesApi.map(category => classMaper(category))
+  } else if (url.includes('subrubros')) {
+    categories = categoriesApi.map(subcategory =>
+      subcategoriesMapper(subcategory)
+    )
+  } else {
+    categories = categoriesApi.map(category => categoriesMapper(category))
+  }
 
   return categories
 }
