d296adf53e103e0c2877f71ab15d12b5be4f6009
RamunnoAJ <ramunnoagustin@gmail.com>
2024-08-16 16:37:32 -0300

    fix: user of type 3 can search himself

--- src/profileClientAccount.js
+++ src/profileClientAccount.js
@@ -2,13 +2,22 @@ import {
   getStorageID,
   getDataFromStorage,
 } from './storage/profileClientAccount.js'
+import { getUserFromStorage } from './storage/storageData.js'
 import { renderProfileClientAccount } from './ui/profileClientAccount.js'
 
 export async function profileClientAccount($profileInfoContainer) {
   $profileInfoContainer.innerHTML = '<span class="loader"></span>'
+  const user = JSON.parse(getUserFromStorage())
 
-  const sellerID = getStorageID()
-  const clients = await getDataFromStorage(sellerID)
+  if (user.role === 3) {
+    renderProfileClientAccount(
+      [{ id: user.idUser, name: user.name }],
+      $profileInfoContainer
+    )
+  } else {
+    const sellerID = getStorageID()
+    const clients = await getDataFromStorage(sellerID)
 
-  renderProfileClientAccount(clients, $profileInfoContainer)
+    renderProfileClientAccount(clients, $profileInfoContainer)
+  }
 }
--- src/profileOrdersHistory.js
+++ src/profileOrdersHistory.js
@@ -1,13 +1,19 @@
 import { getClientsFromSeller } from './api/profileClientList.js'
 import { getStorageID } from './storage/profileClientAccount.js'
+import { getUserFromStorage } from './storage/storageData.js'
 import { renderSelect } from './ui/profileOrdersHistory.js'
 
 export async function profileOrdersHistory(parentElement) {
   parentElement.innerHTML = '<span class="loader"></span>'
 
   const sellerID = getStorageID()
+  const user = JSON.parse(getUserFromStorage())
   if (sellerID) {
-    const clients = await getClientsFromSeller(sellerID)
-    renderSelect(clients, parentElement)
+    if (user.role !== 3) {
+      const clients = await getClientsFromSeller(sellerID)
+      renderSelect(clients, parentElement)
+    } else {
+      renderSelect([{ id: user.idUser, name: user.name }], parentElement)
+    }
   }
 }
