b14e1ec5f29abe08e169964d9400980d0b9f5aee
RamunnoAJ <ramunnoagustin@gmail.com>
2025-02-14 11:28:48 -0300

    feat: modal to show images

--- src/ui/store.js
+++ src/ui/store.js
@@ -257,18 +257,25 @@ function renderProductCard(item, parentElement) {
 function createProductCard(item, user) {
   const $card = document.createElement('article')
   $card.classList = 'store__product__card'
-  let image = item.images[0]
+  const images = item.images.map(i =>
+    i ? 'oagsa' + i.split('oagsa')[1].replace('\\public_html', '') : null
+  )
+
+  let image = images[0]
 
   if (!image) {
-    image = switchImage(item.idCategory.trim())
+    image = getImage(item.idCategory.trim())
   }
-
-  console.log(image)
 
   const $image = document.createElement('img')
   $image.src = 'https://' + image
   $image.alt = item.name
   $image.classList = 'store__product__card__image'
+  $image.addEventListener('click', () => {
+    if (images.length > 0) openImageModal(images)
+    else openImageModal([image])
+  })
+  $image.style.cursor = 'pointer'
   $card.appendChild($image)
 
   const $info = document.createElement('div')
@@ -384,6 +391,74 @@ function createProductCard(item, user) {
   return $card
 }
 
+function openImageModal(images) {
+  let currentIndex = 0
+
+  const $modal = document.createElement('div')
+  $modal.classList = 'image-modal'
+  $modal.innerHTML = `
+    <div class="image-modal__content">
+      <span class="image-modal__close">&times;</span>
+      ${
+        images.length > 1
+          ? "<button class='image-modal__prev'>&lt;</button>"
+          : ''
+      }
+      <img src="${
+        'https://' + images[currentIndex]
+      }" class="image-modal__img" />
+      ${
+        images.length > 1
+          ? "<button class='image-modal__next'>&gt;</button>"
+          : ''
+      }
+      <div class="image-modal__thumbnails"></div>
+    </div>
+  `
+
+  document.body.appendChild($modal)
+
+  const $image = $modal.querySelector('.image-modal__img')
+  const $prevBtn = $modal.querySelector('.image-modal__prev')
+  const $nextBtn = $modal.querySelector('.image-modal__next')
+  const $thumbnailsContainer = $modal.querySelector('.image-modal__thumbnails')
+
+  images.forEach((imgSrc, index) => {
+    const $thumb = document.createElement('img')
+    $thumb.src = 'https://' + imgSrc
+    $thumb.classList = 'image-modal__thumbnail'
+    if (index === currentIndex) $thumb.classList.add('active')
+
+    $thumb.addEventListener('click', () => updateImage(index))
+    $thumbnailsContainer.appendChild($thumb)
+  })
+
+  function updateImage(index) {
+    currentIndex = (index + images.length) % images.length
+    $image.src = 'https://' + images[currentIndex]
+
+    document
+      .querySelectorAll('.image-modal__thumbnail')
+      .forEach(($thumb, i) => {
+        $thumb.classList.toggle('active', i === currentIndex)
+      })
+  }
+
+  if ($prevBtn && $nextBtn) {
+    $prevBtn.addEventListener('click', () => updateImage(currentIndex - 1))
+    $nextBtn.addEventListener('click', () => updateImage(currentIndex + 1))
+  }
+
+  $modal.addEventListener('click', e => {
+    if (
+      e.target.classList.contains('image-modal') ||
+      e.target.classList.contains('image-modal__close')
+    ) {
+      document.body.removeChild($modal)
+    }
+  })
+}
+
 export async function renderDolar() {
   const $dolar = document.querySelector('#dolar')
   const dolar = await getDolar()
@@ -391,59 +466,28 @@ export async function renderDolar() {
   $dolar.textContent = `Valor dolar : $${dolar.value.trim()}`
 }
 
-function switchImage(condition) {
-  switch (condition) {
-    case 'U001':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2011%20-%20HERRAMIENTA.png?alt=media&token=b06bbe3a-cd7e-4a80-a4e7-3bccb9a8df33&_gl=1*15t1cdf*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NTk4MzMyMC4yLjEuMTY4NTk4NDI0Ny4wLjAuMA'
+const IMAGES = {
+  U001: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2011%20-%20HERRAMIENTA.png?alt=media&token=b06bbe3a-cd7e-4a80-a4e7-3bccb9a8df33&_gl=1*15t1cdf*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NTk4MzMyMC4yLjEuMTY4NTk4NDI0Ny4wLjAuMA',
+  U002: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2021%20-%20CASCO%20MOTO.png?alt=media&token=35372e86-cd9c-4142-9987-8922bf8a5bc7&_gl=1*1xq7zgf*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU5OC4wLjAuMA..',
+  O001: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%20%2017%20-%20LUBRICANTE.png?alt=media&token=96998bf8-ecfd-42e4-b05c-8a65967df918&_gl=1*71q4za*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU3MS4wLjAuMA..',
+  R001: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..',
+  R040: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..',
+  R057: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..',
+  R058: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..',
+  Z001: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..',
+  W100: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%20%2017%20-%20LUBRICANTE.png?alt=media&token=96998bf8-ecfd-42e4-b05c-8a65967df918&_gl=1*71q4za*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU3MS4wLjAuMA..',
+  C039: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2019%20-%20CASCOS%20BICI.png?alt=media&token=b056c059-b7b7-434a-87fa-14380ec41cf7&_gl=1*1piegc8*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU5My4wLjAuMA..',
+  F240: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..',
+  F700: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2023%20-%20CADENA%20MOTO.png?alt=media&token=9e1f0d47-58b0-46a9-bbff-698736e9dba2&_gl=1*btavat*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzYwMy4wLjAuMA..',
+  F800: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%201%20-%20BICICLETA.png?alt=media&token=ee32880e-5be8-4ba8-9e88-89e510546bce&_gl=1*19kjrha*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU3Ni4wLjAuMA..',
+  V001: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2023%20-%20CADENA%20MOTO.png?alt=media&token=9e1f0d47-58b0-46a9-bbff-698736e9dba2&_gl=1*btavat*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzYwMy4wLjAuMA..',
+  P090: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2023%20-%20CADENA%20MOTO.png?alt=media&token=9e1f0d47-58b0-46a9-bbff-698736e9dba2&_gl=1*btavat*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzYwMy4wLjAuMA..',
+  P100: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2023%20-%20CADENA%20MOTO.png?alt=media&token=9e1f0d47-58b0-46a9-bbff-698736e9dba2&_gl=1*btavat*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzYwMy4wLjAuMA..',
+  P400: 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2023%20-%20CADENA%20MOTO.png?alt=media&token=9e1f0d47-58b0-46a9-bbff-698736e9dba2&_gl=1*btavat*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzYwMy4wLjAuMA..',
+  DEFAULT:
+    'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2015%20-%20CUBIERTA%20DE%20BICI.png?alt=media&token=b11b4ad8-3be2-4636-bca7-fff24f31e36c&_gl=1*as18e2*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4OC4wLjAuMA..',
+}
 
-    case 'U002':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2021%20-%20CASCO%20MOTO.png?alt=media&token=35372e86-cd9c-4142-9987-8922bf8a5bc7&_gl=1*1xq7zgf*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU5OC4wLjAuMA..'
-
-    case 'O001':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%20%2017%20-%20LUBRICANTE.png?alt=media&token=96998bf8-ecfd-42e4-b05c-8a65967df918&_gl=1*71q4za*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU3MS4wLjAuMA..'
-    case 'R001':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..'
-
-    case 'R040':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..'
-
-    case 'R057':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..'
-
-    case 'R058':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..'
-
-    case 'Z001':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..'
-
-    case 'W100':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%20%2017%20-%20LUBRICANTE.png?alt=media&token=96998bf8-ecfd-42e4-b05c-8a65967df918&_gl=1*71q4za*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU3MS4wLjAuMA..'
-
-    case 'C039':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2019%20-%20CASCOS%20BICI.png?alt=media&token=b056c059-b7b7-434a-87fa-14380ec41cf7&_gl=1*1piegc8*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU5My4wLjAuMA..'
-
-    case 'F240':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2013%20-%20ARTICULOS%20DE%20BICI.png?alt=media&token=89594a59-a8e5-4ad5-956f-6b5617fd59ec&_gl=1*1hygt*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4My4wLjAuMA..'
-
-    case 'F700':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2023%20-%20CADENA%20MOTO.png?alt=media&token=9e1f0d47-58b0-46a9-bbff-698736e9dba2&_gl=1*btavat*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzYwMy4wLjAuMA..'
-
-    case 'F800':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%201%20-%20BICICLETA.png?alt=media&token=ee32880e-5be8-4ba8-9e88-89e510546bce&_gl=1*19kjrha*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU3Ni4wLjAuMA..'
-
-    case 'V001':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2023%20-%20CADENA%20MOTO.png?alt=media&token=9e1f0d47-58b0-46a9-bbff-698736e9dba2&_gl=1*btavat*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzYwMy4wLjAuMA..'
-
-    case 'P090':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2023%20-%20CADENA%20MOTO.png?alt=media&token=9e1f0d47-58b0-46a9-bbff-698736e9dba2&_gl=1*btavat*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzYwMy4wLjAuMA..'
-
-    case 'P100':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2023%20-%20CADENA%20MOTO.png?alt=media&token=9e1f0d47-58b0-46a9-bbff-698736e9dba2&_gl=1*btavat*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzYwMy4wLjAuMA..'
-
-    case 'P400':
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2023%20-%20CADENA%20MOTO.png?alt=media&token=9e1f0d47-58b0-46a9-bbff-698736e9dba2&_gl=1*btavat*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzYwMy4wLjAuMA..'
-
-    default:
-      return 'firebasestorage.googleapis.com/v0/b/oagsa-1d9e9.appspot.com/o/Web%20Oagsa%20Iconos%2FOAGSA%20-%20Iconos%20Web%2015%20-%20CUBIERTA%20DE%20BICI.png?alt=media&token=b11b4ad8-3be2-4636-bca7-fff24f31e36c&_gl=1*as18e2*_ga*MTg4MzM4NDcyNC4xNjg1NzQwOTY2*_ga_CW55HF8NVT*MTY4NjA2NzU2Mi40LjEuMTY4NjA2NzU4OC4wLjAuMA..'
-  }
+function getImage(image) {
+  return IMAGES[image] || IMAGES['DEFAULT']
 }
